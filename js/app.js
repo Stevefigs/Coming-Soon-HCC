ce55ac7aa9af5e0092bff31d407f45e5111550be
Stevefigs <44175088+Stevefigs@users.noreply.github.com>
2020-11-16 20:48:47 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+/* EMAIL FORM HANDLING */
+
+$('.form-button input').click(function(e){
+  // do not submit form
+  e.preventDefault();
+  // save value if the form field is not blank or the default
+  if( $('#email').val() !== '' && $('#email').val() !== 'EMAIL' ){
+    var val = $('#email').val();
+    // add email address to message
+    $('.answer span').text(val);}
+})
+
+//click transition
+
+$('.clicker').click(function(f){
+  //do not transition screen
+f.preventDefault();
+
+if( $('#email').val() !== ''){
+  //transition screen to completed message
+  $('.main').addClass('open');
+  }
+
+}) 
+
+//clears value="EMAIL"
+
+$('input').click(function(){
+  $('#email').attr('value', '');
+  })
+
